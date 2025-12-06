5e0fb28c9e909e4d43d35c5642afb18f824274e4
Josh <j.p.lu@outlook.com>
2025-12-06 22:58:03 +1100

    feat: spending by day tooltip

--- apps/web/components/vis/category/spending-by-day.tsx
+++ apps/web/components/vis/category/spending-by-day.tsx
@@ -1,9 +1,43 @@
 'use client';
 
+import { getColorClassName } from '@/lib/chart';
 import { getCategorySpendingByTimestamp } from '@/lib/db/spending';
-import { formatCurrency, formatValueInBaseUnits } from '@/lib/ui';
+import { cn, formatCurrency, formatValueInBaseUnits } from '@/lib/ui';
 import { use } from 'react';
-import { AreaChart } from '../../ui/area-chart';
+import { AreaChart, TooltipProps } from '../../ui/area-chart';
+
+const Tooltip = ({ active, payload }: TooltipProps) => {
+  if (!active || !payload || payload.length === 0) return null;
+
+  return (
+    <div
+      className={cn(
+        // base
+        'rounded-md border text-sm shadow-md',
+        // border color
+        'border-gray-200 dark:border-gray-800',
+        // background color
+        'bg-white dark:bg-gray-950',
+        // layout
+        'flex items-stretch gap-2 p-2'
+      )}
+    >
+      <div className={cn('w-1 rounded-xl', getColorClassName('blue', 'bg'))} />
+      <div className="flex flex-col gap-1">
+        <p className="font-medium text-gray-900 dark:text-gray-50">
+          {payload[0].value
+            ? formatCurrency(formatValueInBaseUnits(payload[0].value), {
+                decimals: 2,
+              })
+            : ''}
+        </p>
+        <p className="whitespace-nowrap text-gray-700 dark:text-gray-300">
+          {payload[0].index}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const SpendingByDay = ({
   dataFetch,
@@ -15,6 +49,7 @@ const SpendingByDay = ({
   return (
     <AreaChart
       className="h-48"
+      customTooltip={Tooltip}
       data={data}
       index="timestamp"
       categories={['value']}
