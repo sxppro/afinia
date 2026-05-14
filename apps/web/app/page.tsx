6dbcf4f901c30cc9712efd8ecfa223b535de040f
Josh <j.p.lu@outlook.com>
2026-05-15 00:54:58 +1000

    feat: uplift homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,17 @@
+import { Button } from '@/components/ui/button';
+import { siteConfig } from '@/lib/siteConfig';
+import Link from 'next/link';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button asChild>
+        <Link href={siteConfig.baseLinks.appHome} className="m-auto">
+          Go to dashboard
+        </Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
