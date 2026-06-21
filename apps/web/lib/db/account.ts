dac2688dc87c9cb549a399f98e4201f743c1c649
Josh <j.p.lu@outlook.com>
2026-06-21 21:52:01 +1000

    fix: account balance

--- apps/web/lib/db/account.ts
+++ apps/web/lib/db/account.ts
@@ -1,8 +1,9 @@
 import { accountTable } from 'afinia-common/schema';
-import { sum } from 'drizzle-orm';
+import { isNull, sum } from 'drizzle-orm';
 import { db } from './client';
 
 export const getAccountBalance = () =>
   db
     .select({ value: sum(accountTable.value_in_base_units).mapWith(Number) })
-    .from(accountTable);
+    .from(accountTable)
+    .where(isNull(accountTable.deleted_at));
