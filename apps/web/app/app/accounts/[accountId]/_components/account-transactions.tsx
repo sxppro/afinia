c43d155c3b495e2a658ac0869e4d7a4a1e8c91b2
Josh <j.p.lu@outlook.com>
2026-07-03 02:09:42 +1000

    fix: suspend account txns

--- apps/web/app/app/accounts/[accountId]/_components/account-transactions.tsx
+++ apps/web/app/app/accounts/[accountId]/_components/account-transactions.tsx
@@ -1,4 +1,7 @@
+import { Skeleton } from '@/components/ui/skeleton';
 import TransactionList from '@/components/vis/transaction/transaction-list';
+import { DEFAULT_PAGE_SIZE } from '@/lib/constants';
+import { Suspense } from 'react';
 
 const AccountTransactions = async ({
   params,
@@ -8,11 +11,23 @@ const AccountTransactions = async ({
   const { accountId } = await params;
 
   return (
-    <TransactionList
-      options={{
-        filters: { account_id: Number(accountId), include_transfers: true },
-      }}
-    />
+    <Suspense
+      fallback={
+        <>
+          {[...Array(DEFAULT_PAGE_SIZE)].map((_, i) => (
+            <Skeleton className="h-12 w-full" key={i} />
+          ))}
+        </>
+      }
+    >
+      <TransactionList
+        options={{
+          limit: DEFAULT_PAGE_SIZE,
+          filters: { account_id: Number(accountId), include_transfers: true },
+        }}
+        isInfinite
+      />
+    </Suspense>
   );
 };
 
